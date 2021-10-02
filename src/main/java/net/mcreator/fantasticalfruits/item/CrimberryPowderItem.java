b75ee5b98a136ff64f7b497019ccb6280cb97928
nehp-d <Nehp@192.168.0.186>
2021-10-02 00:58:34 -0300

    crimberry tweaks and powder

---
+++ src/main/java/net/mcreator/fantasticalfruits/item/CrimberryPowderItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.fantasticalfruits.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.fantasticalfruits.FantasticalFruitsModElements;
+
+@FantasticalFruitsModElements.ModElement.Tag
+public class CrimberryPowderItem extends FantasticalFruitsModElements.ModElement {
+	@ObjectHolder("fantastical_fruits:crimberry_powder")
+	public static final Item block = null;
+	public CrimberryPowderItem(FantasticalFruitsModElements instance) {
+		super(instance, 19);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("crimberry_powder");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/fantasticalfruits/procedures/CrimberryFoodEatenProcedure.java
@@ -0,0 +1,23 @@
+package net.mcreator.fantasticalfruits.procedures;
+
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.fantasticalfruits.FantasticalFruitsMod;
+
+import java.util.Map;
+
+public class CrimberryFoodEatenProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				FantasticalFruitsMod.LOGGER.warn("Failed to load dependency entity for procedure CrimberryFoodEaten!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (entity instanceof LivingEntity)
+			((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.FIRE_RESISTANCE, (int) 60, (int) 1));
+	}
+}
