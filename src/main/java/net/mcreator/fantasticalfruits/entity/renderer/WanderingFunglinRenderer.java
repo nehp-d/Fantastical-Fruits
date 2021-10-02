5acc2932abf96cfe237487bfcd94d75b2777d025
fruitologistero <gsill@192.168.254.20>
2021-10-02 15:04:56 -0500

    funglin

---
+++ src/main/java/net/mcreator/fantasticalfruits/entity/renderer/WanderingFunglinRenderer.java
@@ -0,0 +1,119 @@
+package net.mcreator.fantasticalfruits.entity.renderer;
+
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.Entity;
+import net.minecraft.client.renderer.model.ModelRenderer;
+import net.minecraft.client.renderer.entity.model.EntityModel;
+import net.minecraft.client.renderer.entity.MobRenderer;
+
+import net.mcreator.fantasticalfruits.entity.WanderingFunglinEntity;
+
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+@OnlyIn(Dist.CLIENT)
+public class WanderingFunglinRenderer {
+	public static class ModelRegisterHandler {
+		@SubscribeEvent
+		@OnlyIn(Dist.CLIENT)
+		public void registerModels(ModelRegistryEvent event) {
+			RenderingRegistry.registerEntityRenderingHandler(WanderingFunglinEntity.entity, renderManager -> {
+				return new MobRenderer(renderManager, new Modelcustom_model(), 0.5f) {
+					@Override
+					public ResourceLocation getEntityTexture(Entity entity) {
+						return new ResourceLocation("fantastical_fruits:textures/funglintexture.png");
+					}
+				};
+			});
+		}
+	}
+
+	// Made with Blockbench 3.9.3
+	// Exported for Minecraft version 1.15 - 1.16 with MCP mappings
+	// Paste this class into your mod and generate all required imports
+	public static class Modelcustom_model extends EntityModel<Entity> {
+		private final ModelRenderer root;
+		private final ModelRenderer rightleg;
+		private final ModelRenderer leftleg;
+		private final ModelRenderer body;
+		private final ModelRenderer leftarm;
+		private final ModelRenderer rightarm;
+		private final ModelRenderer head;
+		private final ModelRenderer mushroom;
+		private final ModelRenderer backpack;
+		public Modelcustom_model() {
+			textureWidth = 64;
+			textureHeight = 64;
+			root = new ModelRenderer(this);
+			root.setRotationPoint(0.0F, 24.0F, 0.0F);
+			rightleg = new ModelRenderer(this);
+			rightleg.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(rightleg);
+			rightleg.setTextureOffset(0, 26).addBox(-2.0F, -4.0F, -1.0F, 2.0F, 4.0F, 2.0F, 0.0F, false);
+			leftleg = new ModelRenderer(this);
+			leftleg.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(leftleg);
+			leftleg.setTextureOffset(22, 0).addBox(0.0F, -4.0F, -1.0F, 2.0F, 4.0F, 2.0F, 0.0F, false);
+			body = new ModelRenderer(this);
+			body.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(body);
+			body.setTextureOffset(0, 18).addBox(-2.0F, -10.0F, -1.0F, 4.0F, 6.0F, 2.0F, 0.0F, false);
+			leftarm = new ModelRenderer(this);
+			leftarm.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(leftarm);
+			leftarm.setTextureOffset(20, 20).addBox(2.0F, -10.0F, -1.0F, 2.0F, 6.0F, 2.0F, 0.0F, false);
+			rightarm = new ModelRenderer(this);
+			rightarm.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(rightarm);
+			rightarm.setTextureOffset(12, 20).addBox(-4.0F, -10.0F, -1.0F, 2.0F, 6.0F, 2.0F, 0.0F, false);
+			head = new ModelRenderer(this);
+			head.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(head);
+			head.setTextureOffset(12, 29).addBox(-2.0F, -13.0F, -2.0F, 4.0F, 3.0F, 3.0F, 0.0F, false);
+			mushroom = new ModelRenderer(this);
+			mushroom.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(mushroom);
+			mushroom.setTextureOffset(0, 0).addBox(-4.0F, -15.0F, -3.0F, 8.0F, 2.0F, 6.0F, 0.0F, false);
+			mushroom.setTextureOffset(14, 14).addBox(-3.0F, -17.0F, -2.0F, 6.0F, 2.0F, 4.0F, 0.0F, false);
+			backpack = new ModelRenderer(this);
+			backpack.setRotationPoint(0.0F, 0.0F, 0.0F);
+			root.addChild(backpack);
+			backpack.setTextureOffset(46, 0).addBox(-3.0F, -12.0F, 1.0F, 6.0F, 7.0F, 3.0F, 0.0F, false);
+			backpack.setTextureOffset(56, 12).addBox(-3.0F, -11.0F, -2.0F, 1.0F, 1.0F, 3.0F, 0.0F, false);
+			backpack.setTextureOffset(46, 12).addBox(-3.0F, -10.0F, -2.0F, 1.0F, 5.0F, 1.0F, 0.0F, false);
+			backpack.setTextureOffset(56, 12).addBox(2.0F, -11.0F, -2.0F, 1.0F, 1.0F, 3.0F, 0.0F, false);
+			backpack.setTextureOffset(46, 12).addBox(2.0F, -10.0F, -2.0F, 1.0F, 5.0F, 1.0F, 0.0F, false);
+			backpack.setTextureOffset(46, 11).addBox(-2.0F, -8.0F, -2.0F, 4.0F, 1.0F, 1.0F, 0.0F, false);
+		}
+
+		@Override
+		public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue,
+				float alpha) {
+			root.render(matrixStack, buffer, packedLight, packedOverlay);
+		}
+
+		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+			modelRenderer.rotateAngleX = x;
+			modelRenderer.rotateAngleY = y;
+			modelRenderer.rotateAngleZ = z;
+		}
+
+		public void setRotationAngles(Entity e, float f, float f1, float f2, float f3, float f4) {
+			this.head.rotateAngleY = f3 / (180F / (float) Math.PI);
+			this.head.rotateAngleX = f4 / (180F / (float) Math.PI);
+			this.rightleg.rotateAngleX = MathHelper.cos(f * 1.0F) * -1.0F * f1;
+			this.rightarm.rotateAngleX = MathHelper.cos(f * 0.6662F) * f1;
+			this.leftleg.rotateAngleX = MathHelper.cos(f * 1.0F) * 1.0F * f1;
+			this.leftarm.rotateAngleX = MathHelper.cos(f * 0.6662F + (float) Math.PI) * f1;
+			this.mushroom.rotateAngleY = f3 / (180F / (float) Math.PI);
+			this.mushroom.rotateAngleX = f4 / (180F / (float) Math.PI);
+		}
+	}
+}
