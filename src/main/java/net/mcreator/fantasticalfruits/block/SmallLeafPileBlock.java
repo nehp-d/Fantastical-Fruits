0358b3736be6783a6b8160c1ec44165ff1e67afd
fruitologistero <gsill@192.168.254.20>
2021-10-03 23:33:28 -0500

    leaf piles

--- src/main/java/net/mcreator/fantasticalfruits/block/SmallLeafPileBlock.java
+++ src/main/java/net/mcreator/fantasticalfruits/block/SmallLeafPileBlock.java
@@ -1,18 +1,61 @@
 
 package net.mcreator.fantasticalfruits.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.DefaultFlowersFeature;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.BlockClusterFeatureConfig;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.Direction;
+import net.minecraft.potion.Effects;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.fantasticalfruits.FantasticalFruitsModElements;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
 
 @FantasticalFruitsModElements.ModElement.Tag
 public class SmallLeafPileBlock extends FantasticalFruitsModElements.ModElement {
-
 	@ObjectHolder("fantastical_fruits:small_leaf_pile")
 	public static final Block block = null;
-
 	public SmallLeafPileBlock(FantasticalFruitsModElements instance) {
 		super(instance, 45);
-
 		MinecraftForge.EVENT_BUS.register(this);
 		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
 	}
@@ -28,12 +71,9 @@ public class SmallLeafPileBlock extends FantasticalFruitsModElements.ModElement 
 	public void clientLoad(FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
 	}
-
 	private static Feature<BlockClusterFeatureConfig> feature = null;
 	private static ConfiguredFeature<?, ?> configuredFeature = null;
-
 	private static class FeatureRegisterHandler {
-
 		@SubscribeEvent
 		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
 			feature = new DefaultFlowersFeature(BlockClusterFeatureConfig.field_236587_a_) {
@@ -46,37 +86,27 @@ public class SmallLeafPileBlock extends FantasticalFruitsModElements.ModElement 
 				public boolean generate(ISeedReader world, ChunkGenerator generator, Random random, BlockPos pos, BlockClusterFeatureConfig config) {
 					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
 					boolean dimensionCriteria = false;
-
 					if (dimensionType == World.OVERWORLD)
 						dimensionCriteria = true;
-
 					if (!dimensionCriteria)
 						return false;
-
 					return super.generate(world, generator, random, pos, config);
 				}
 			};
-
 			configuredFeature = feature
 					.withConfiguration(
 							(new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(block.getDefaultState()), new SimpleBlockPlacer()))
 									.tries(3).build())
 					.withPlacement(Features.Placements.VEGETATION_PLACEMENT).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).func_242731_b(1);
-
 			event.getRegistry().register(feature.setRegistryName("small_leaf_pile"));
 			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("fantastical_fruits:small_leaf_pile"), configuredFeature);
 		}
-
 	}
-
 	@SubscribeEvent
 	public void addFeatureToBiomes(BiomeLoadingEvent event) {
-
 		event.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION).add(() -> configuredFeature);
 	}
-
 	public static class BlockCustomFlower extends FlowerBlock {
-
 		public BlockCustomFlower() {
 			super(Effects.SPEED, 5, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().sound(SoundType.PLANT)
 					.hardnessAndResistance(0f, 0f).setLightLevel(s -> 0));
@@ -95,7 +125,6 @@ public class SmallLeafPileBlock extends FantasticalFruitsModElements.ModElement 
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
@@ -106,7 +135,5 @@ public class SmallLeafPileBlock extends FantasticalFruitsModElements.ModElement 
 		public PlantType getPlantType(IBlockReader world, BlockPos pos) {
 			return PlantType.PLAINS;
 		}
-
 	}
-
 }
--- src/main/java/net/mcreator/fantasticalfruits/item/VermillionMangoItem.java
+++ src/main/java/net/mcreator/fantasticalfruits/item/VermillionMangoItem.java
@@ -1,12 +1,21 @@
 
 package net.mcreator.fantasticalfruits.item;
 
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+import net.mcreator.fantasticalfruits.itemgroup.FantasticalFruitsItemGroup;
+import net.mcreator.fantasticalfruits.FantasticalFruitsModElements;
+
 @FantasticalFruitsModElements.ModElement.Tag
 public class VermillionMangoItem extends FantasticalFruitsModElements.ModElement {
-
 	@ObjectHolder("fantastical_fruits:vermillion_mango")
 	public static final Item block = null;
-
 	public VermillionMangoItem(FantasticalFruitsModElements instance) {
 		super(instance, 43);
 	}
@@ -15,14 +24,10 @@ public class VermillionMangoItem extends FantasticalFruitsModElements.ModElement
 	public void initElements() {
 		elements.items.add(() -> new FoodItemCustom());
 	}
-
 	public static class FoodItemCustom extends Item {
-
 		public FoodItemCustom() {
 			super(new Item.Properties().group(FantasticalFruitsItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON)
-					.food((new Food.Builder()).hunger(6).saturation(3.4f)
-
-							.build()));
+					.food((new Food.Builder()).hunger(6).saturation(3.4f).build()));
 			setRegistryName("vermillion_mango");
 		}
 
@@ -30,7 +35,5 @@ public class VermillionMangoItem extends FantasticalFruitsModElements.ModElement
 		public UseAction getUseAction(ItemStack itemstack) {
 			return UseAction.EAT;
 		}
-
 	}
-
 }
