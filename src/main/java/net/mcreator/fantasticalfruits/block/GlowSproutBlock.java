550e5ead0751587454a6d1334d3fc48f8118b881
nehp-d <Nehp@192.168.0.186>
2021-10-09 15:33:41 -0300

    • added maple variants

--- src/main/java/net/mcreator/fantasticalfruits/block/GlowSproutBlock.java
+++ src/main/java/net/mcreator/fantasticalfruits/block/GlowSproutBlock.java
@@ -1,18 +1,68 @@
 
 package net.mcreator.fantasticalfruits.block;
 
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.PlantType;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.gen.feature.Features;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.DefaultFlowersFeature;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.BlockClusterFeatureConfig;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.blockplacer.SimpleBlockPlacer;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.IWorldReader;
+import net.minecraft.world.ISeedReader;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.registry.WorldGenRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.Direction;
+import net.minecraft.potion.Effects;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.SoundEvent;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.FlowerBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.fantasticalfruits.itemgroup.FantasticalNatureItemGroup;
+import net.mcreator.fantasticalfruits.FantasticalFruitsModElements;
+
+import java.util.Random;
+import java.util.List;
+import java.util.Collections;
 
 @FantasticalFruitsModElements.ModElement.Tag
 public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
-
 	@ObjectHolder("fantastical_fruits:glow_sprout")
 	public static final Block block = null;
-
 	public GlowSproutBlock(FantasticalFruitsModElements instance) {
 		super(instance, 129);
-
 		MinecraftForge.EVENT_BUS.register(this);
 		FMLJavaModLoadingContext.get().getModEventBus().register(new FeatureRegisterHandler());
 	}
@@ -29,12 +79,9 @@ public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
 	public void clientLoad(FMLClientSetupEvent event) {
 		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
 	}
-
 	private static Feature<BlockClusterFeatureConfig> feature = null;
 	private static ConfiguredFeature<?, ?> configuredFeature = null;
-
 	private static class FeatureRegisterHandler {
-
 		@SubscribeEvent
 		public void registerFeature(RegistryEvent.Register<Feature<?>> event) {
 			feature = new DefaultFlowersFeature(BlockClusterFeatureConfig.field_236587_a_) {
@@ -47,37 +94,27 @@ public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
 				public boolean generate(ISeedReader world, ChunkGenerator generator, Random random, BlockPos pos, BlockClusterFeatureConfig config) {
 					RegistryKey<World> dimensionType = world.getWorld().getDimensionKey();
 					boolean dimensionCriteria = false;
-
 					if (dimensionType == World.OVERWORLD)
 						dimensionCriteria = true;
-
 					if (!dimensionCriteria)
 						return false;
-
 					return super.generate(world, generator, random, pos, config);
 				}
 			};
-
 			configuredFeature = feature
 					.withConfiguration(
 							(new BlockClusterFeatureConfig.Builder(new SimpleBlockStateProvider(block.getDefaultState()), new SimpleBlockPlacer()))
 									.tries(40).build())
 					.withPlacement(Features.Placements.VEGETATION_PLACEMENT).withPlacement(Features.Placements.HEIGHTMAP_PLACEMENT).func_242731_b(2);
-
 			event.getRegistry().register(feature.setRegistryName("glow_sprout"));
 			Registry.register(WorldGenRegistries.CONFIGURED_FEATURE, new ResourceLocation("fantastical_fruits:glow_sprout"), configuredFeature);
 		}
-
 	}
-
 	@SubscribeEvent
 	public void addFeatureToBiomes(BiomeLoadingEvent event) {
-
 		event.getGeneration().getFeatures(GenerationStage.Decoration.VEGETAL_DECORATION).add(() -> configuredFeature);
 	}
-
 	public static class BlockCustomFlower extends FlowerBlock {
-
 		public BlockCustomFlower() {
 			super(Effects.GLOWING, 5, Block.Properties.create(Material.PLANTS).doesNotBlockMovement().sound(SoundType.NETHER_WART)
 					.hardnessAndResistance(0f, 0f).setLightLevel(s -> 0));
@@ -87,11 +124,7 @@ public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
 		@Override
 		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
 			Vector3d offset = state.getOffset(world, pos);
-			return VoxelShapes.or(makeCuboidShape(0, 0, 0, 15, 10, 15)
-
-			)
-
-					.withOffset(offset.x, offset.y, offset.z);
+			return VoxelShapes.or(makeCuboidShape(0, 0, 0, 15, 10, 15)).withOffset(offset.x, offset.y, offset.z);
 		}
 
 		@Override
@@ -111,7 +144,6 @@ public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
 
 		@Override
 		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-
 			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
 			if (!dropsOriginal.isEmpty())
 				return dropsOriginal;
@@ -120,14 +152,10 @@ public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
 
 		@Override
 		public boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos) {
-
 			Block ground = state.getBlock();
 			return (ground == Blocks.STONE || ground == Blocks.GRANITE || ground == Blocks.DIORITE || ground == Blocks.ANDESITE
-					|| ground == Blocks.MYCELIUM || ground == Blocks.COARSE_DIRT || ground == Blocks.COBBLESTONE || ground == Blocks.MOSSY_COBBLESTONE
-
-			)
-
-			;
+					|| ground == Blocks.MYCELIUM || ground == Blocks.COARSE_DIRT || ground == Blocks.COBBLESTONE
+					|| ground == Blocks.MOSSY_COBBLESTONE);
 		}
 
 		@Override
@@ -135,7 +163,6 @@ public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
 			BlockPos blockpos = pos.down();
 			BlockState groundState = worldIn.getBlockState(blockpos);
 			Block ground = groundState.getBlock();
-
 			return this.isValidGround(groundState, worldIn, blockpos);
 		}
 
@@ -143,7 +170,5 @@ public class GlowSproutBlock extends FantasticalFruitsModElements.ModElement {
 		public PlantType getPlantType(IBlockReader world, BlockPos pos) {
 			return PlantType.CAVE;
 		}
-
 	}
-
 }
