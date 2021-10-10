32ed0464f5c70216d2da481f527f9426168f0d2c
nehp-d <Nehp@192.168.0.186>
2021-10-10 14:35:53 -0300

    • added maple variants recipes • added wooden tools out of aspen

--- src/main/java/net/mcreator/fantasticalfruits/procedures/GlowSproutConditionProcedure.java
+++ src/main/java/net/mcreator/fantasticalfruits/procedures/GlowSproutConditionProcedure.java
@@ -33,6 +33,7 @@ public class GlowSproutConditionProcedure {
 		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
-		return ((world.getLight(new BlockPos((int) x, (int) y, (int) z))) < 8);
+		return (((world.getLight(new BlockPos((int) x, (int) y, (int) z))) < 8)
+				&& (!(world.canBlockSeeSky(new BlockPos((int) x, (int) y, (int) z)))));
 	}
 }
