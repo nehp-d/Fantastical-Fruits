76b52d74a8791731136bf9d0918fe392cc993faa
fruitologistero <gsill@192.168.254.20>
2021-10-19 23:35:26 -0500

    arctic stone

---
+++ src/main/java/net/mcreator/fantasticalfruits/procedures/ArcticStoneChangeProcedure.java
@@ -0,0 +1,47 @@
+package net.mcreator.fantasticalfruits.procedures;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.fantasticalfruits.block.ArcticStoneBlock;
+import net.mcreator.fantasticalfruits.FantasticalFruitsMod;
+
+import java.util.Map;
+
+public class ArcticStoneChangeProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				FantasticalFruitsMod.LOGGER.warn("Failed to load dependency x for procedure ArcticStoneChange!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				FantasticalFruitsMod.LOGGER.warn("Failed to load dependency y for procedure ArcticStoneChange!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				FantasticalFruitsMod.LOGGER.warn("Failed to load dependency z for procedure ArcticStoneChange!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				FantasticalFruitsMod.LOGGER.warn("Failed to load dependency world for procedure ArcticStoneChange!");
+			return;
+		}
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == ArcticStoneBlock.block)) {
+			{
+				BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+				BlockState _bs = Blocks.STONE.getDefaultState();
+				world.setBlockState(_bp, _bs, 3);
+			}
+		}
+	}
+}
