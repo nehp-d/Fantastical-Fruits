63d82c2e8508177c41dbd2969a5e3037e8c754ed
nehp-d <Nehp@192.168.0.186>
2021-10-22 20:50:58 -0300

    • spongefruit stuff • fixed some bugs

--- src/main/java/net/mcreator/fantasticalfruits/procedures/WharpleyPlantProcedure.java
+++ src/main/java/net/mcreator/fantasticalfruits/procedures/WharpleyPlantProcedure.java
@@ -7,14 +7,19 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.GameType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Hand;
 import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
+import net.minecraft.client.Minecraft;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 
@@ -86,6 +91,24 @@ public class WharpleyPlantProcedure {
 				.getItem() == WharpleySeedsItem.block)) {
 			if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == Blocks.SOUL_SAND)) {
 				if (((world.getBlockState(new BlockPos((int) x, (int) (y + 1), (int) z))).getBlock() == Blocks.AIR)) {
+					if ((!(new Object() {
+						public boolean checkGamemode(Entity _ent) {
+							if (_ent instanceof ServerPlayerEntity) {
+								return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.CREATIVE;
+							} else if (_ent instanceof PlayerEntity && _ent.world.isRemote()) {
+								NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+										.getPlayerInfo(((AbstractClientPlayerEntity) _ent).getGameProfile().getId());
+								return _npi != null && _npi.getGameType() == GameType.CREATIVE;
+							}
+							return false;
+						}
+					}.checkGamemode(entity)))) {
+						if (entity instanceof PlayerEntity) {
+							ItemStack _stktoremove = new ItemStack(WharpleySeedsItem.block);
+							((PlayerEntity) entity).inventory.func_234564_a_(p -> _stktoremove.getItem() == p.getItem(), (int) 1,
+									((PlayerEntity) entity).container.func_234641_j_());
+						}
+					}
 					world.setBlockState(new BlockPos((int) x, (int) (y + 1), (int) z), WharpleyCrop0Block.block.getDefaultState(), 3);
 					if (world instanceof World && !world.isRemote()) {
 						((World) world).playSound(null, new BlockPos((int) x, (int) y, (int) z),
