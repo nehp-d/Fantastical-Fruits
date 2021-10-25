4fe9ca9a3ba0c6f45a4988d983ed0df664a93477
nehp-d <Nehp@192.168.0.186>
2021-10-24 22:21:06 -0300

    • pp

--- src/main/java/net/mcreator/fantasticalfruits/procedures/RepulsionProcedureProcedure.java
+++ src/main/java/net/mcreator/fantasticalfruits/procedures/RepulsionProcedureProcedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.fantasticalfruits.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+
+import net.minecraft.world.World;
 import net.minecraft.world.IWorld;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
@@ -17,8 +22,36 @@ import net.mcreator.fantasticalfruits.FantasticalFruitsMod;
 
 import java.util.Map;
 import java.util.Iterator;
+import java.util.HashMap;
 
 public class RepulsionProcedureProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onEntityAttacked(LivingAttackEvent event) {
+			if (event != null && event.getEntity() != null) {
+				Entity entity = event.getEntity();
+				Entity sourceentity = event.getSource().getTrueSource();
+				Entity imediatesourceentity = event.getSource().getImmediateSource();
+				double i = entity.getPosX();
+				double j = entity.getPosY();
+				double k = entity.getPosZ();
+				double amount = event.getAmount();
+				World world = entity.world;
+				Map<String, Object> dependencies = new HashMap<>();
+				dependencies.put("x", i);
+				dependencies.put("y", j);
+				dependencies.put("z", k);
+				dependencies.put("amount", amount);
+				dependencies.put("world", world);
+				dependencies.put("entity", entity);
+				dependencies.put("sourceentity", sourceentity);
+				dependencies.put("imediatesourceentity", imediatesourceentity);
+				dependencies.put("event", event);
+				executeProcedure(dependencies);
+			}
+		}
+	}
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
