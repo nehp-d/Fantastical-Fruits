913967d241918a30eb5aac80998f86c4879d61a4
nehp-d <Nehp@192.168.0.186>
2021-10-27 00:22:06 -0300

    jooniper bough

---
+++ src/main/java/net/mcreator/fantasticalfruits/item/JuniperBranchItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.fantasticalfruits.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+
+import net.mcreator.fantasticalfruits.itemgroup.FantasticalItemsItemGroup;
+import net.mcreator.fantasticalfruits.FantasticalFruitsModElements;
+
+@FantasticalFruitsModElements.ModElement.Tag
+public class JuniperBranchItem extends FantasticalFruitsModElements.ModElement {
+	@ObjectHolder("fantastical_fruits:juniper_branch")
+	public static final Item block = null;
+	public JuniperBranchItem(FantasticalFruitsModElements instance) {
+		super(instance, 256);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(FantasticalItemsItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(3).saturation(3f).build()));
+			setRegistryName("juniper_branch");
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.EAT;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(Items.STICK);
+			super.onItemUseFinish(itemstack, world, entity);
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
+		}
+	}
+}
