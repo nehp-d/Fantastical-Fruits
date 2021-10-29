3dabff7cc46294ad2e6553a24509311464d9d7cc
nehp-d <Nehp@192.168.0.186>
2021-10-29 20:52:54 -0300

    absorptive brew B(

---
+++ src/main/java/net/mcreator/fantasticalfruits/AbsorptiveBrewRecipeBrewingRecipe.java
@@ -0,0 +1,48 @@
+
+package net.mcreator.fantasticalfruits;
+
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.common.brewing.IBrewingRecipe;
+import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
+
+import net.minecraft.potion.Potions;
+import net.minecraft.potion.PotionUtils;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+
+import net.mcreator.fantasticalfruits.item.SandsunsItem;
+import net.mcreator.fantasticalfruits.item.AbsorptiveBrewItem;
+
+@FantasticalFruitsModElements.ModElement.Tag
+public class AbsorptiveBrewRecipeBrewingRecipe extends FantasticalFruitsModElements.ModElement {
+	public AbsorptiveBrewRecipeBrewingRecipe(FantasticalFruitsModElements instance) {
+		super(instance, 283);
+	}
+
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+		BrewingRecipeRegistry.addRecipe(new CustomBrewingRecipe());
+	}
+	public static class CustomBrewingRecipe implements IBrewingRecipe {
+		@Override
+		public boolean isInput(ItemStack input) {
+			Item inputItem = input.getItem();
+			return (inputItem == Items.POTION || inputItem == Items.SPLASH_POTION || inputItem == Items.LINGERING_POTION)
+					&& PotionUtils.getPotionFromItem(input) == Potions.AWKWARD;
+		}
+
+		@Override
+		public boolean isIngredient(ItemStack ingredient) {
+			return ingredient.getItem() == SandsunsItem.block;
+		}
+
+		@Override
+		public ItemStack getOutput(ItemStack input, ItemStack ingredient) {
+			if (isInput(input) && isIngredient(ingredient)) {
+				return new ItemStack(AbsorptiveBrewItem.block);
+			}
+			return ItemStack.EMPTY;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/fantasticalfruits/item/AbsorptiveBrewItem.java
@@ -0,0 +1,94 @@
+
+package net.mcreator.fantasticalfruits.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.UseAction;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.fantasticalfruits.procedures.AbsorptiveBrewEffectProcedure;
+import net.mcreator.fantasticalfruits.itemgroup.FantasticalFruitsItemGroup;
+import net.mcreator.fantasticalfruits.FantasticalFruitsModElements;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+
+@FantasticalFruitsModElements.ModElement.Tag
+public class AbsorptiveBrewItem extends FantasticalFruitsModElements.ModElement {
+	@ObjectHolder("fantastical_fruits:absorptive_brew")
+	public static final Item block = null;
+	public AbsorptiveBrewItem(FantasticalFruitsModElements instance) {
+		super(instance, 282);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(FantasticalFruitsItemGroup.tab).maxStackSize(1).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(0).saturation(0f).setAlwaysEdible().build()));
+			setRegistryName("absorptive_brew");
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack itemstack) {
+			return UseAction.DRINK;
+		}
+
+		@Override
+		public net.minecraft.util.SoundEvent getEatSound() {
+			return net.minecraft.util.SoundEvents.ENTITY_GENERIC_DRINK;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("\u00A79Absorption (3:00)"));
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = new ItemStack(Items.GLASS_BOTTLE);
+			super.onItemUseFinish(itemstack, world, entity);
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				AbsorptiveBrewEffectProcedure.executeProcedure($_dependencies);
+			}
+			if (itemstack.isEmpty()) {
+				return retval;
+			} else {
+				if (entity instanceof PlayerEntity) {
+					PlayerEntity player = (PlayerEntity) entity;
+					if (!player.isCreative() && !player.inventory.addItemStackToInventory(retval))
+						player.dropItem(retval, false);
+				}
+				return itemstack;
+			}
+		}
+	}
+}
